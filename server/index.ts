64201e6f35010373219709fd301dbdc093a01515
nando-z <57473087+nando-z@users.noreply.github.com>
2025-05-21 01:34:05 +0300

    Feat(Auth): setup new pakages

--- server/index.ts
+++ server/index.ts
@@ -6,22 +6,23 @@ import morgan from "morgan";
 import { abort } from "./routes/abort";
 import { HomeRoute } from "@/test/HomeRouteTest";
 import { createServer } from "node:http";
+import cookieParser from "cookie-parser";
 export const PORT = process.env.PORT;
 export const app = express();
 export const server = createServer(app)
-dotenv.config();
-app.use()
+dotenv.config() ;
+app.use(cookieParser());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors("*"
+));
 app.use(
   helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
     crossOriginOpenerPolicy: false,
     crossOriginResourcePolicy: false,
-    xXssProtection: true,
   }),
 );
 app.get("/", HomeRoute);
--- test/HomeRouteTest.ts
+++
@@ -1,5 +0,0 @@
-import { RequestHandler } from "./http";
-
-export const HomeRoute: RequestHandler = (_req, res) => {
-  res.status(200).send({ message: "Hello Test File" });
-};
